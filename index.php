c0b57dbdcba11361b4b316fd0cb799fe6a3d1508
Shawn <caleman@gmail.com>
2018-09-05 16:34:47 -0600

    initial Commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+# $argv will contain the event object. You can output its contents like this if you like
+#var_export($argv, true);
+
+printf('Go Serverless v1.0! Your PHP function executed successfully!');
+?>
+
